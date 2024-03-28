669f38ea889eac9f42d1537df1f27bdb551a16f7
vladvavilov <vladvav94@gmail.com>
2024-03-28 18:55:48 +0500

    add sync func

--- nested-chat-plugin-sdk/sdk.py
+++ nested-chat-plugin-sdk/sdk.py
@@ -1,9 +1,11 @@
-from plugin_sdk.schemes import SyncRequest
+from schemes import SyncRequest
 from typing import Callable
 from fastapi import APIRouter, Request, HTTPException
+import httpx
 class PluginRouter(APIRouter):
-    def __init__(self, api_url: str):
+    def __init__(self, api_url: str=None, plugin_name: str = None):
         super().__init__()
+        self.plugin_name = plugin_name
         self.api_url = api_url
         self.create_handler = None
         self.update_handler = None
@@ -25,6 +27,12 @@ class PluginRouter(APIRouter):
     def on_execute(self, handler: Callable):
         self.execute_handler = handler
         self.add_api_route("/execute", self._handle_execute, methods=["POST"])
+
+    async def sync(self) -> int:
+        async with httpx.AsyncClient() as client:
+            payload = {"name": self.plugin_name}
+            response = await client.post(self.api_url, json=payload)
+            return response.status_code
 
     async def _handle_create(self, request_data: SyncRequest):
         if not self.create_handler:
